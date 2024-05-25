89ff099c59108ec5c589004ad12621d4fcaa4e11
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-05-24 21:05:43 -0400

    Modificación del JS

--- game.js
+++ game.js
@@ -4,12 +4,14 @@ const TecladoDir = document.querySelector(".Teclado");
 
 // Creando Botones del Teclado
 for (let i = 97; i <= 122; i++) {
+    //console.log(String.fromCharCode(i));
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     TecladoDir.appendChild(button);
 }
 
 
+/*
 <button>a</button><button>b</button><button>c</button><button>d</button>
                 <button>e</button><button>f</button><button>g</button><button>h</button>
                 <button>i</button><button>j</button><button>k</button><button>l</button>
@@ -17,4 +19,4 @@ for (let i = 97; i <= 122; i++) {
                 <button>q</button><button>r</button><button>s</button><button>t</button>
                 <button>u</button><button>v</button><button>w</button><button>x</button>
                 <button>y</button><button>z</button>
-            
+*/
