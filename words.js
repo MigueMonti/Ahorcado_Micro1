e6043e8b5cc9bd460177708ef693049e54107344
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-05-24 22:19:33 -0400

    Modificación del JS

---
+++ words.js
@@ -0,0 +1,42 @@
+const PalabrasAdivinar = [
+    {
+        palabra: "Universidad",
+        clave: "Institución."
+    },
+    {
+        palabra: "Algoritmos",
+        clave: "Materia."
+    },
+    {
+        palabra: "Saman",
+        clave: "UNIMET."
+    },
+    {
+        palabra: "CAVUM",
+        clave: "Agrupación."
+    },
+    {
+        palabra: "Avila",
+        clave: "Caracas."
+    },
+    {
+        palabra: "Feria",
+        clave: "Almuerzo."
+    },
+    {
+        palabra: "Batidos",
+        clave: "Juice Bar."
+    },
+    {
+        palabra: "Pepperoni",
+        clave: "Pizza."
+    },
+    {
+        palabra: "Empanadas",
+        clave: "Punto Grill."
+    },
+    {
+        palabra: "Te",
+        clave: "La Tiendita."
+    },
+];
