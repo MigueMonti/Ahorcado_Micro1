982c3499d6c6ec6f4fd583bf51edebddcb363bbe
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-05-24 20:56:12 -0400

    Creación del JS

---
+++ game.js
@@ -0,0 +1,20 @@
+
+
+const TecladoDir = document.querySelector(".Teclado");
+
+// Creando Botones del Teclado
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    TecladoDir.appendChild(button);
+}
+
+
+<button>a</button><button>b</button><button>c</button><button>d</button>
+                <button>e</button><button>f</button><button>g</button><button>h</button>
+                <button>i</button><button>j</button><button>k</button><button>l</button>
+                <button>m</button><button>n</button><button>o</button><button>p</button>
+                <button>q</button><button>r</button><button>s</button><button>t</button>
+                <button>u</button><button>v</button><button>w</button><button>x</button>
+                <button>y</button><button>z</button>
+            
